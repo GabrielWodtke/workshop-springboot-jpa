b6a5c6b90add9dd5b4938ee9bec330a553f073f6
Gabriel Wodtke <gabrielwodtke28@gmail.com>
2025-12-15 16:06:01 -0300

    Order implementation pt. 1.1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,6 +3,8 @@ package com.educandoweb.course.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
 
     public User() {
     }
@@ -29,6 +35,9 @@ public class User implements Serializable {
 
     public Long getId() {
         return id;
+    }
+    public List<Order> getOrders() {
+        return orders;
     }
 
     public void setId(Long id) {
