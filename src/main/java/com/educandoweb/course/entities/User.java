e9be1e8643514d37e775709e30040f1a8f64d070
Gabriel Wodtke <gabrielwodtke28@gmail.com>
2025-12-23 12:54:12 -0300

    User delete

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -11,8 +11,8 @@ import java.util.Objects;
 @Entity
 @Table(name="tb_user")
 public class User implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
